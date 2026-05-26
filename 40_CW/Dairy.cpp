9e6c060295414fd2871a5a904ca65d9fd2d10864
paulbardar <Q.C.tulip@gmail.com>
2026-05-26 19:42:47 +0300

    Add dairy item show Dairy

--- 40_CW/Dairy.cpp
+++ 40_CW/Dairy.cpp
@@ -2,6 +2,12 @@
 
 Dairy::Dairy()
 {
+	Book[Date()].push_back("C++");
+	Book[Date()].push_back("Home work");
+	Book[Date(2026, 12, 31)].push_back("New year");
+	Book[Date() + 2].push_back("C++");
+	Book[Date() -1 ].push_back("C++");
+	Book[Date() -1 ].push_back("Home work");
 }
 
 void Dairy::addEvent(Date d, string info)
@@ -22,4 +28,11 @@ void Dairy::showEventByInfo(string info) const&
 
 void Dairy::showAllEvents() const&
 {
+	for (auto item : Book) {
+		cout << item.first << endl;
+		for (int i = 0; i < item.second.size(); i++) {
+			cout << "\t" << i + 1 << ". " << item.second[i] << endl;
+		}
+		cout << endl;
+	}
 }
